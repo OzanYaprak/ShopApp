09a1063aa43697b9b44c79126fb4b9d70215ef0b
ozanyaprak <ozanerhanyaprak@icloud.com>
2023-02-18 20:03:17 +0300

    Entity katmanı düzenlemesi yapıldı.

---
+++ ShopApp.Entities/ProductCategory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopApp.Entities
+{
+    public class ProductCategory
+    {
+        public int ProductCategoryID { get; set; }
+        public int CategoryID { get; set; }
+        public int ProductID { get; set; }
+
+
+        //navigations
+        public Category Category { get; set; }
+        public Product Product { get; set; }
+    }
+}
